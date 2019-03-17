a2d5a17b696aa7663d5a441d4ed0a5eaf96e73f2
Julien Blandineau <julien.blandineau@asso-cocktail.fr>
2019-03-17 18:14:58 +0100

    List vers Deque

--- src/main/java/kata/rpn/MyCalculator.java
+++ src/main/java/kata/rpn/MyCalculator.java
@@ -1,9 +1,8 @@
 package kata.rpn;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiFunction;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.stream.Stream;
 
 public class MyCalculator implements Calculator {
@@ -11,25 +10,22 @@ public class MyCalculator implements Calculator {
     @Override
     public BigDecimal compute(String operation) {
         String[] ops = operation.split(" ");
-
-        List<BigDecimal> numbers = new ArrayList<>();
+        Deque<BigDecimal> numbers = new ArrayDeque<>();
         Stream.of(ops).forEach(s -> {
             if (isOperator(s)) {
-                BigDecimal right = numbers.remove(numbers.size() - 1);
-                BigDecimal left = numbers.remove(numbers.size() - 1);
-                numbers.add(calc(left, right, s));
+                BigDecimal right = numbers.pop();
+                BigDecimal left = numbers.pop();
+                numbers.push(calc(left, right, s));
             } else {
-                numbers.add(new BigDecimal(s));
+                numbers.push(new BigDecimal(s));
             }
         });
 
-        return numbers.get(0);
+        return numbers.getFirst();
     }
 
-
-
-    private BigDecimal calc(BigDecimal left, BigDecimal right, String s) {
-        switch (s) {
+    private BigDecimal calc(BigDecimal left, BigDecimal right, String operation) {
+        switch (operation) {
             case "+":
                 return left.add(right);
             case "-":
@@ -42,7 +38,7 @@ public class MyCalculator implements Calculator {
                 }
                 return left.divide(right);
             default:
-                throw new IllegalArgumentException("Opération inconnue " + s);
+                throw new IllegalArgumentException("Opération inconnue " + operation);
         }
     }
 
